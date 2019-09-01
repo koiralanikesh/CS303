e05964cc99deb17083b57356eabf4c7f89c16f02
Nikesh Koirala <nkmgm@mail.umkc.edu>
2019-09-01 11:42:18 -0500

    Final

--- Question 2/Source/Question 2.cpp
+++ Question 2/Source/Question 2.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 int main()
 {
-//variable declaration
+//introducing the integers
 int n, y1, y2;
-for (n=0; n<=100; n=n+10)
+//initiating a forloop to check the comparison with increment of 10
+for(n=0;n<=100;n=n+10)
 {
-	y1=100*n+10;
-	y2=5*n+2;
-	//Comparing the values y1 with y2
-if (y1>y2)
-	std::cout<<"The value of y1 is greater than y2 when n is "<<n<<"\n";
-else if (y1==y2)
-	std::cout<<"The value of y1 is equal to y2 when n is "<<n<<"\n";
-else if (y1<y2)
-	std::cout<<"The value of y1 is less than y2 when n is "<<n<<"\n";
+y1=100*n+10;
+y2=5*n*n+2;
+//comparing the value of y1 and y2
+if(y1>y2)
+std::cout<<"y1 is greater than y2 for n = "<<n<<"\n";
+else if(y1<y2)
+std::cout<<"y1 is less than y2 for n = "<<n<<"\n";
+else if(y1==y2)
+std::cout<<"y1 and y2 are equal for n = "<<n<<"\n";
 }
 return 0;
 }
